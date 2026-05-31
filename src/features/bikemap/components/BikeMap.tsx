b6b121853301fe12bb5fe3b83e7ea6592f91ed0f
Felipe Becker <ofelipebecker@proton.me>
2026-05-31 18:00:02 -0300

    feat: Initialize Mapbox GL instance in BikeMap component
    Issue: CTB-2605A

--- src/features/bikemap/components/BikeMap.tsx
+++ src/features/bikemap/components/BikeMap.tsx
@@ -1,8 +1,28 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { bikeMapConfig } from '../utils/bikeMapConfig.ts';
 import '../styles/BikeMap.scss';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 const BikeMap = () => {
   const bikeMapContainerRef = useRef<HTMLDivElement | null>(null);
+  const bikeMapRef = useRef<mapboxgl.Map | null>(null);
+
+  useEffect(() => {
+    if (!bikeMapContainerRef.current) return;
+
+    const bikeMapInstance = new mapboxgl.Map({
+      container: bikeMapContainerRef.current,
+      style: bikeMapConfig.style.url,
+      center: bikeMapConfig.center.coordinates,
+      zoom: bikeMapConfig.center.zoom,
+      maxBounds: bikeMapConfig.bounds.maxBounds,
+    });
+
+    bikeMapRef.current = bikeMapInstance;
+  }, []);
 
   return <div id='bike-map-container' ref={bikeMapContainerRef} />;
 };

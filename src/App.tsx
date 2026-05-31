77d823bf14aed2bacf4b67144881092eece8020b
Felipe Becker <ofelipebecker@proton.me>
2026-05-31 17:23:12 -0300

    feat: Integrate BikeMap component into App layout
    Issue: CTB-2605A

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import LayersContextProvider from './store/layers-visibility-context.tsx';
+import BikeMap from './features/bikemap/components/BikeMap.tsx';
 import MainNav from './features/nav/MainNav.tsx';
 
 function App() {
   return (
     <LayersContextProvider>
       <MainNav />
+      <BikeMap />
     </LayersContextProvider>
   );
 }

757ee0a22bee0ec714305b89236bf119cc99f92f
Felipe Becker <ofelipebecker@proton.me>
2026-05-31 14:36:43 -0300

    refactor: Migrate global styles from CSS to SCSS with Bootstrap import
    Issue: CTB-2605A

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import LayersContextProvider from './store/layers-visibility-context.tsx';
 import MainNav from './features/nav/MainNav.tsx';
 
-import './App.css';
-
 function App() {
   return (
     <LayersContextProvider>

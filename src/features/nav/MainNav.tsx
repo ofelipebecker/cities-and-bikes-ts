489374a263ad5bc1c434ad3e5f679a084ce158a4
Felipe Becker <ofelipebecker@proton.me>
2026-06-08 12:20:50 -0300

    ui: Add beta badge next to logo in main navigation
    Issue: CTB-2605A

--- src/features/nav/MainNav.tsx
+++ src/features/nav/MainNav.tsx
@@ -1,4 +1,4 @@
-import { Container, Image, Navbar, Offcanvas } from 'react-bootstrap';
+import { Badge, Container, Image, Navbar, Offcanvas } from 'react-bootstrap';
 import logo from '../../assets/images/logo-ctb-horizontal-dark-pt.svg';
 import LayersMenu from './LayersMenu.tsx';
 
@@ -6,8 +6,11 @@ const MainNav = () => (
   <Navbar expand={false} className='bg-cyan-500 shadow-1'>
     <Container fluid>
       <Navbar.Toggle className='fs-3' />
-      <Navbar.Brand href='/' className='me-auto ms-3'>
+      <Navbar.Brand href='/' className='d-flex me-auto ms-3'>
         <Image src={logo} />
+        <Badge pill bg='dark' className='ms-2 align-self-start text-cyan-100'>
+          beta
+        </Badge>
       </Navbar.Brand>
       <Navbar.Offcanvas placement='start' className='bg-gray-200'>
         <Offcanvas.Header closeButton className='align-items-start'>
